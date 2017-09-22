4c915e307c2549be51930c54eafaf6e05e3cf42d
Max Koren <Koren_Max@bah.com>
2017-09-22 16:30:57 -0400

    slider??

--- script.js
+++ script.js
@@ -148,8 +148,40 @@ app.controller('Controller', ['$scope', '$http', function ($scope, $http) {
 			default:
 		}
     };
-
 }]);
+
+app.directive('slider', function($document) {
+    return function(scope, element, attrs) {
+		$element.on('mousedown', function(event) {
+			event.preventDefault();
+			$document.on('mousemove', mousemove);
+			$document.on('mouseup', mouseup);
+		});
+		
+		function mousemove(event) {
+			var x = event.pageX;
+			if ($attrs.resizerMax && x > $attrs.resizerMax) {
+				x = parseInt($attrs.resizerMax);
+			}
+			$element.css({
+				left: x + 'px'
+			});
+			$($attrs.resizerLeft).css({
+				width: x + 'px'
+			});
+			$($attrs.resizerRight).css({
+				left: (x + parseInt($attrs.resizerWidth)) + 'px'
+			});
+		}
+
+		function mouseup() {
+			$document.unbind('mousemove', mousemove);
+			$document.unbind('mouseup', mouseup);
+		}
+	};
+});
+
+
 
 function CardList(comparator, name) {
     this.contents = [];
